e7ce82ea9629f8576b2acdd2b8fb05f1733a993e
tunaparlak0 <tuna.parlak@ogr.sakarya.edu.tr>
2026-01-13 02:33:19 +0300

    Player eklendi, hareket kodu yazildi ve input ayari duzeltildi

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f; // Hız ayarı
+    Rigidbody2D rb;
+    Vector2 movement;
+
+    void Start()
+    {
+        // Rigidbody'yi otomatik bul
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        // Klavyeden yönleri al
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        // Hareketi uygula
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
